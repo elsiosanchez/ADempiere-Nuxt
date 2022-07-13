3fe2ca06d0626619a770f8fa8f8add7bcb2056ed
elsiosanchez <elsiosanches@gmail.com>
2022-07-13 14:57:44 -0400

    initial commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ count }} <br>
+    <el-input-number v-model="count" :min="1" :max="10" />
+    <hr>
+  </div>
+  <!-- <h1> The_Header1 </h1> -->
+   <!-- <br> -->
+  <!-- <el-input-number v-model="count" :min="1" :max="10" /> -->
+</template>
+
+<script setup lang="ts">
+import { ElInputNumber } from "element-plus";
+const count = 0;
+// import {
+//   ElButton
+// } from "element-plus";
+const buttons = [
+  { type: '', text: 'plain' },
+  { type: 'primary', text: 'primary' },
+  { type: 'success', text: 'success' },
+  { type: 'info', text: 'info' },
+  { type: 'warning', text: 'warning' },
+  { type: 'danger', text: 'danger' },
+];
+
+</script>
